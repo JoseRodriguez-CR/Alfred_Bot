19d14dea6163f87db4f14e5a7747a511e8b8c6ff
JoseRodriguez-CR <josemariorodriguezc@gmail.com>
2021-10-18 18:29:16 -0600

    Uploading Files

---
+++ AlfredQuotes.java
@@ -0,0 +1,78 @@
+
+import java.util.Date;
+public class AlfredQuotes {
+    /*
+    * Inputs: None
+    * Return Type: String
+    * Description: Returns a generic greeting that Alfred will say to anyone.
+    */
+    public String basicGreeting() {
+        // You do not need to code here, this is an example method
+        return "Hello, lovely to see you. How are you?";
+    }
+    
+    /* 
+    * Inputs: String name, String dayPeriod 
+    *                      Assume "morning", "afternoon" or "evening".
+    * Return Type: String
+    * Description: Returns a greeting that includes the person's name
+    *         as well as the day period. For example:
+    *        "Good evening, Beth Kane. Lovely to see you."
+    * Tip: Try using the String.format method for string interpolation in Java.
+    */
+    public String guestGreeting(String name, String dayPeriod) {
+        // YOUR CODE HERE
+        return "Good " + dayPeriod + ", " + name + "!";
+    }
+    /* 
+    * Inputs: None
+    * Return Type: String
+    * Description: Returns a polite announcement of the current date.
+    */
+    public String dateAnnouncement() {
+        // YOUR CODE HERE
+        Date date = new Date();
+        return "According your request, current date is: "+ date;
+    }
+    
+    /*********************************************
+    Final Challenge!
+    Alfred is listening. Write a method that accepts any string of conversation. 
+    If "Alexis" appears in the conversation return a snarky response, such as:
+    "Right away, sir. She certainly isn't sophisticated enough for that." 
+    
+    If "Alfred" is in the conversation return an obliging response, for example: 
+    "At your service. As you wish, naturally." 
+    
+    If neither name is found, return an appropriate response, for instance:
+    "Right. And with that I shall retire." 
+    **********************************************/
+    /*
+    * Inputs: String (A conversation)
+    * Return Type: String (Something Alfred would say in response)
+    * 
+    * Tip: Use the indexOf String method
+    */
+    public String respondBeforeAlexis(String conversation) {
+        // YOUR CODE HERE
+        if(conversation.indexOf( "Alexis" )> -1){
+            return "For sure sir, let me help with that before having you waiting a lot.";
+        }
+        if(conversation.indexOf( "Alfred" )> -1){
+            return "At your service, since Alfred know that I'm going to find it as well!";
+        }
+        return "In that case, my presence is not required here, I shall retire rigth now!";
+    }
+    // NINJA BONUS
+    // Write your own AlfredQuote method using any of the String methods you have learned!
+
+    public String respondAlfredBotYelling(String conversation) {
+        if(conversation.indexOf( "Good Work" )> -1){
+            return "My glad to help you sir!!";
+        }
+        if(conversation.indexOf( "Bad Work" )> -1){
+            return conversation.toUpperCase() + "Excuse sir, Am I wrong or you are blame on me?";
+        }
+        return "ENOUGH, THAT'S ALL FOR TODAY!";
+    }
+}
